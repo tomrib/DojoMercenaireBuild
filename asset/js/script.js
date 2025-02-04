778edec401e40333ce22ff0b32e71e714d822c8f
tomrib <ribeiro.tommy.60@gmail.com>
2025-02-04 11:15:00 +0100

    ajout de model pour build tank

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,16 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const boutons = document.querySelectorAll('button');
+    const boutons = document.querySelectorAll('.boutonHome button');
     const contenus = document.querySelectorAll('.contenu');
 
     boutons.forEach(bouton => {
         bouton.addEventListener('click', function () {
-            const idContenu = this.id.replace('bouton', 'contenu'); // Récupérer l'ID du contenu associé
+            const idContenu = "contenu" + this.id.replace('bouton', '');
 
-            // Masquer tous les contenus
             contenus.forEach(contenu => contenu.classList.remove('active'));
+            boutons.forEach(btn => btn.classList.remove('active'));
 
-            // Afficher le contenu correspondant au bouton cliqué
             document.getElementById(idContenu).classList.add('active');
+            this.classList.add('active');
+        });
+    });
+
+    const tankButtons = document.querySelectorAll('#tank1, #tank2, #tank3, #tank4, #tank5, #tank6');
+    const tankContents = document.querySelectorAll('.boxTank');
+
+    tankButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const idContenu = "boxTank" + this.id.replace('tank', '');
+
+            tankContents.forEach(content => content.classList.remove('active'));
+            tankButtons.forEach(btn => btn.classList.remove('active'));
+
+            document.getElementById(idContenu).classList.add('active');
+            this.classList.add('active');
         });
     });
 });
